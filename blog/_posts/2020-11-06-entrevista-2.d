ea7a59b6adae37df9edd8e534de4f305637ba7da
EstudiantesRSEF <estudiantes@rsef.es>
2020-10-23 19:03:20 +0200

    Create 2020-11-06-entrevista-2.d

---
+++ blog/_posts/2020-11-06-entrevista-2.d
@@ -0,0 +1,84 @@
+# Segunda parte
+
+### Alejandro - Nos podrías comentar tu paso por la universidad.
+
+Empecé Química en la Universidad Complutense de Madrid, y en el segundo año te
+permitían simultanear, de forma que empecé primero de Física, estando en segundo de
+Química. En los años siguientes hacía mi curso de Química y unas cuantas asignaturas de
+Física. Hasta que luego acabe Químicas en los años que establecía el programa, y una
+vez acabé, me puse en serio con la Física a la vez que con mi doctorado en Química-Física.
+En Química hice mi la especialidad en Química Fundamental, centrándome y
+orientándome en Química-Ffísica, de forma que siempre he estado muy en contacto
+con la Física. Y en Física hice la especialidad de Física de Materiales.
+Una formación converge con la otra, y bueno ahora yo trabajo en
+Física de la Materia Blanda, donde los aspectos físico-químicos
+químicos y biofísicos del sistema determinan sus propiedades.
+Entonces al final fFui buscando lo que quería hacer, dentro de unas licenciaturas que no tenían
+unos planes de estudio tan especializados. Según avanza tu carrera sigues buscando tu sitio, y
+mi sitio era un lugar en medio de las dos disciplinas.
+
+
+En cuanto a los hobbies la verdad que mi vida ha sido siempre la ciencia. Siempre me ha
+gustado mucho, y es a lo que dedicaba mi tiempo. Si que tengo un grupo de amigos desde que
+era pequeña y hemos salido juntos, quedado, y he tenido una vida muy normal en ese aspecto.
+De estudiar cuando toca estudiar porque tocan exámenes, pero el resto del tiempo ser una
+estudiante normal.
+
+### Alberto - Alguna vez a lo largo de tu carrera has podido sentirte frustrada cuando no has
+alcanzado ciertos objetivos, ciertos resultados.
+
+Si, yo creo que la frustración es algo que acompaña a nuestra profesión a diario. Por cada éxito
+tienes un montón de fracasos, esto es así. Por ejemplo, tardé cerca de un año en
+desarrollar la técnica en la que ahora baso todos mis modelos, la
+microfluídica de vesículas. Estuve un año pegándome con
+unos dispositivos que no funcionan, que no hacían lo que yo quería… Y bueno, mi supervisor
+en Harvard de ese tiempo me dijo inténtalo 3 o 4 meses y si no sale ponte a hacer otra cosa, puesto que otros lo han intentado
+antes…. Y bueno, al final salió.
+
+La frustración es algo que acompaña a la investigación. Y yo siempre se lo digo
+a los estudiantes que trabajan conmigo, que por cada cosa que sale, hay muchas otras que no.
+Pero cuando sale, la satisfacción es tan grande, que hace que quieras seguir con este gusanillo,
+y te piques con la investigación. Sobre todo durante el tiempo de la tesis doctoral, te das
+cuenta si esto es para ti o si no. Si el problema te obsesiona y sigues preguntándote como
+responder a ese problema, entonces es que eso te motiva. Si eso no ocurre durante tu tesis
+doctoral, no va a ocurrir después.
+
+### Miriam - Yo quería preguntarte, como lograste enfocar tu carrera. ¿Como conseguiste
+encontrar esa cosa que te motivara tanto?
+
+Bueno, yo la verdad que he tenido unos mentores estupendos. Entonces claro, dar con la
+gente adecuada y que esta gente tenga un grupo de colaboradores extenso que te enseñen a
+mirar al mundo de manera diferente y a hacerte las preguntas correctas, eso ayuda mucho. Yo
+te diría que busques un científico de referencia que pueda ejercer esa función de mentor,
+porque con él y con todo ese grupo de colaboradores y gente que tenga, encontrarás tu sitio. Y lo que si que hay que hacer siempre es hablar con cuanta más
+gente mejor. Y aunque empieces a hacer una investigación en un grupo, si no te llena, pasar al
+grupo siguiente, y si no al siguiente. Porque al final esto consiste en buscar la cosa que te
+motiva, porque es donde vas a dar lo mejor de ti.
+
+### Has mostrado siempre esa pasión por la ciencia y el querer dedicarte a la
+investigación. Pero seguro que varias personas que nos leen están acabando el grado o
+haciendo el master, y se estén planteando hacer un doctorado. ¿Cómo les ayudarías a ellos a
+decidir si hacer o no un doctorado?
+
+Yo creo que tienen la oportunidad de verlo a través de los trabajos de fin de grado (TFG) y
+trabajos de fin de master (TFM). El TFG a veces es más bibliográfico y sólo didáctico, pero el
+TFM es en general (aunque depende de la universidad) más exploratorio. Aquí
+se puede ver como es el trabajo de investigación y como es trabajar en él. Yo creo que aquí se
+tiene una buena oportunidad para ver el funcionamiento del grupo y ver que tipo de cosas se
+requieren para dedicarse a la investigación. En esa etapa es un buen momento
+de hablar con todos los investigadores y profesores al alcance, porque los investigadores y profesores
+somos gente abierta y cercana. Si un estudiante te pone un email para poder hablar de
+esto, tú lo vas a dar todo para tener una conversación e intentarle clarificar como es esta
+profesión.
+
+### Miriam - ¿Recomendarías a jóvenes investigadores que salieran y que vieran ellos mismos
+como funciona todo?
+
+No es solo que lo recomiende, si no que creo que es necesario para tu desarrollo como
+científico que salgas y veas como se trabaja en otros sitios. No puedes basarte solo en lo que
+se hace aquí. Y de hecho, cada vez es un requisito más indispensable para conseguir una
+posición de investigador aquí el que hayas salido. Es un requisito a nivel estatal para ir
+consiguiendo becas públicas, el que te hayas movido. Además, es súper enriquecedor, y es la
+única manera de aprender a hacer ciencia. Y no solo la ciencia de tu primer y único mentor, si
+no tener varios, para ir desarrollando tu propio estilo, y poder abarcar problemas más
+amplios.
