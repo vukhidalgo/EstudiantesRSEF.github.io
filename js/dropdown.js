914dedae9d254e725428dccbefa9cd65e04d46d3
MiDV81 <83724465+MiDV81@users.noreply.github.com>
2023-09-10 22:40:29 +0200

    Hover and Below origin false -> true

--- js/dropdown.js
+++ js/dropdown.js
@@ -12,9 +12,9 @@
       inDuration: 300,
       outDuration: 225,
       constrain_width: true, // Constrains width of dropdown to the activator
-      hover: false,
+      hover: true,
       gutter: 0, // Spacing from edge
-      belowOrigin: false,
+      belowOrigin: true,
       alignment: 'left'
     };
 
