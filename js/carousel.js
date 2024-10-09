916aaa727cbe422db6c617736424156e20738c34
isivoxx <118601028+isivoxx@users.noreply.github.com>
2024-10-09 20:37:33 +0200

    Update carousel.js

--- js/carousel.js
+++ js/carousel.js
@@ -15,3 +15,43 @@ function showPrevImage() {
 
 document.querySelector('.next').addEventListener('click', showNextImage);
 document.querySelector('.prev').addEventListener('click', showPrevImage);
+
+// Tu código anterior aquí...
+
+// Código para el deslizamiento del carrusel
+const carousel = document.querySelector('.carousel');
+let isDragging = false;
+let startX, scrollLeft;
+
+carousel.addEventListener('mousedown', (e) => {
+    isDragging = true;
+    startX = e.pageX - carousel.offsetLeft;
+    scrollLeft = carousel.scrollLeft;
+});
+
+carousel.addEventListener('mouseleave', () => {
+    isDragging = false;
+});
+
+carousel.addEventListener('mouseup', () => {
+    isDragging = false;
+});
+
+carousel.addEventListener('mousemove', (e) => {
+    if (!isDragging) return;
+    e.preventDefault();
+    const x = e.pageX - carousel.offsetLeft;
+    const walk = (x - startX) * 2;
+    carousel.scrollLeft = scrollLeft - walk;
+});
+
+// Para pantallas táctiles
+carousel.addEventListener('touchstart', (e) => {
+    isDragging = true;
+    startX = e.touches[0].pageX - carousel.offsetLeft;
+    scrollLeft = carousel.scrollLeft;
+});
+
+carousel.addEventListener('touchend', () => {
+    isDragging = fa
+
