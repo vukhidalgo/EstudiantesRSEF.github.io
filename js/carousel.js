6a47f375d6ebe895d600370324f1f078e1d34bb5
isivoxx <118601028+isivoxx@users.noreply.github.com>
2024-10-14 20:54:16 +0200

    Update carousel.js

--- js/carousel.js
+++ js/carousel.js
@@ -1,21 +1,3 @@
-let currentIndex = 0;
-const items = document.querySelectorAll('.carousel-item');
-
-function showNextImage() {
-    items[currentIndex].classList.remove('active');
-    currentIndex = (currentIndex + 1) % items.length;
-    items[currentIndex].classList.add('active');
-}
-
-function showPrevImage() {
-    items[currentIndex].classList.remove('active');
-    currentIndex = (currentIndex - 1 + items.length) % items.length;
-    items[currentIndex].classList.add('active');
-}
-
-document.querySelector('.next').addEventListener('click', showNextImage);
-document.querySelector('.prev').addEventListener('click', showPrevImage);
-
 const carousel = document.querySelector('.carousel');
 let isDragging = false;
 let startX;
